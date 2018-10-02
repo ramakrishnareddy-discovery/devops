f89c862a588e3d4526e1394d889dd70bf692ee1a
r.satti <r.satti@accenture.com>
2018-10-02 12:54:06 +0530

    Added code to copy code from Acc GIT to Qualcomm GitHub

---
+++ copyrepo/migrateAccGitToGithub.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+if [ $# -eq 0 ] || [ "$1" == "-?" ] || [ "$1" == "--help" ] || ["$1" == "-h" ]
+  then
+    echo "Usage: `basename $0` <USERNAME> <PROJECT_NAME> <REPO_ NAME> <GITHUB_USERNAME>"
+    echo "e.g. `basename $0` r.satti APPQ common c-rsatti"
+    exit 1
+fi
+
+
+if [ ! -d "./${2}/${3}" ] ; then
+  echo "cloning" ${3}
+  git clone --mirror https://${1}@innersource.accenture.com/scm/${2}/${3}.git ./${2}/${3}
+fi
+
+  cd ./${2}/${3}
+  git fetch
+
+  git remote add dpgithub https://github.qualcomm.com/${4}/${3}.git
+  git push --mirror dpgithub
