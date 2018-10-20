bf290cab37f47651e8d23d9dd3264e466526f91a
r.satti <r.satti@accenture.com>
2018-10-20 15:10:29 +0530

    UPDATE

--- copyrepo/deleteIMAGEStag.sh
+++ copyrepo/deleteIMAGEStag.sh
@@ -3,13 +3,13 @@
 # THIS SCRIPT ACCEPTS COMMA SEPARATED DOCKER IMAGE TAGS AND REMOVE ONE BY ONE.
  
 # Docker Image deletion is two step process. 1) delete ALL THE MENTIONED IMAGE TAGS 2) delete respective(whose tag already deleted) image. 
-# STEP 1) ./<CURRENT_SCRIPT_NAME>.sh 10.0.31.225:5000 qualcomm-ms-demandplanner 0.snapshot.89,0.snapshot.90,3.5-10.98
+# STEP 1) ./<CURRENT_SCRIPT_NAME>.sh 10.0.31.225:5000 CLIENT-ms-demandplanner 0.snapshot.89,0.snapshot.90,3.5-10.98
 # SETP 2) docker exec -it registry bin/registry garbage-collect /etc/docker/registry/config.yml
 
 set -e
 if [ "$#" -eq "0" ] || [ "$1" == "-?" ] || [ "$1" == "--help" ];then
     echo "Usage: `basename $0` <DOCKER_PRIVATE_REGISTRY_IP:PORT> <BINARY_NAME> <TAG1>,<TAG2>,<TAG3>,<TAG4>"
-    echo "e.g. `basename $0` 10.0.31.225:5000 qualcomm-ms-demandplanner 0.snapshot.89,4.05-12,,3.12.0"
+    echo "e.g. `basename $0` 10.0.31.225:5000 CLIENT-ms-demandplanner 0.snapshot.89,4.05-12,,3.12.0"
     exit 1
 fi
 
--- copyrepo/deleteSpecificImageTag.sh
+++ copyrepo/deleteSpecificImageTag.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 # Docker Image deletion is two step process. 1) delete image tag 2) delete respective(whose tag already deleted) image. 
-# STEP 1) ./<CURRENT_SCRIPT_NAME>.sh 10.0.31.225:5000 qualcomm-ms-demandplanner 0.snapshot.89
+# STEP 1) ./<CURRENT_SCRIPT_NAME>.sh 10.0.31.225:5000 CLIENT-ms-demandplanner 0.snapshot.89
 # SETP 2) docker exec -it registry bin/registry garbage-collect /etc/docker/registry/config.yml
 
 if [ "$#" -eq "0" ] || [ "$1" == "-?" ] || [ "$1" == "--help" ];then
     echo "Usage: `basename $0` <DOCKER_PRIVATE_REGISTRY_IP:PORT> <BINARY_NAME> <TAG>"
-    echo "e.g. `basename $0` 10.0.31.225:5000 qualcomm-ms-demandplanner 0.snapshot.89"
+    echo "e.g. `basename $0` 10.0.31.225:5000 CLIENT-ms-demandplanner 0.snapshot.89"
     exit 1
 fi
 
