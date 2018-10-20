9cbb90350a23516723c0f82373d65428a52f879c
r.satti <r.satti@accenture.com>
2018-10-20 15:14:20 +0530

    UPDATE

--- copyrepo/deleteImageTag.sh
+++ copyrepo/deleteImageTag.sh
@@ -7,7 +7,7 @@
 set -e
 if [ "$#;" -eq "0" ] || [ "$1" == "-?" ] || [ "$1" == "--help" ];then
     echo "Usage: `basename $0` <DOCKER_PRIVATE_REGISTRY_IP:PORT> <BINARY_NAME>"
-    echo "e.g. `basename $0` 10.0.31.225:5000 qualcomm-ms-demandplanner"
+    echo "e.g. `basename $0` 10.0.31.225:5000 CLIENT-ms-demandplanner"
     exit 1
 fi
 curl -v -sSL -X DELETE "http://${1}/v2/${2}/manifests/$(
