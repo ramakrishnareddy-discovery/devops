ac3c3e7d7d84903adb92427419d930184ef3e6c3
r.satti <r.satti@accenture.com>
2018-10-20 15:42:05 +0530

    updated migrateAccGitToGithub script

--- copyrepo/migrateAccGitToGithub.sh
+++ copyrepo/migrateAccGitToGithub.sh
@@ -2,7 +2,7 @@
 # This script will create mirror of the source GIT Repository REFERENCES i.e. copy all branches/tags REFERENCES to CLIENT GIT. 
 
 # PREREQUISITE: git should be installed using $sudo yum install git -y
-#set -e 
+set -e 
 if [ "$#" -eq "0" ] || [ "$1" == "-?" ] || [ "$1" == "--help" ];then
     echo "Usage: `basename $0` <USERNAME> <PROJECT_NAME> <REPO_ NAME> <GITHUB_USERNAME>"
     echo "e.g. `basename $0` r.satti APPQ common c-rsatti"
@@ -18,5 +18,5 @@ fi
   cd ./${2}/${3}
   git fetch
 
-  git remote add dpgithub https://github.<CLIENT_GIT>.com/${4}/${3}.git
+  git remote set-url dpgithub https://github.<CLIENT_GIT>.com/${4}/${3}.git
   git push --mirror dpgithub
